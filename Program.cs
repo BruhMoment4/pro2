0282799327fd82dfae3ac6bfa96d41f846a9992f
BruhMoment4 <54839701+BruhMoment4@users.noreply.github.com>
2019-09-12 12:10:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace opdracht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.Clear();
+     
+            Console.WriteLine(#############################################)
+            Console.WriteLine(#*#*#*#*#*#*                     *#*#*#*#*#*#)
+            Console.WriteLine(*#*#*#*#*#*#       MY GAME!      #*#*#*#*#*#*)
+            Console.WriteLine(#*#*#*#*#*#*                     *#*#*#*#*#*#)
+            Console.WriteLine(#############################################)
+          
+            Console.ReadLine();
+
+        } 
+
+
+
+    }
+}
